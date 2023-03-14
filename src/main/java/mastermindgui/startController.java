5ae21dbd73eff345ef4025456e5a4419c0a24fcd
Sten Kristian Høitomt <97731056+hoitomt1@users.noreply.github.com>
2023-03-14 18:34:48 +0100

    Fjernet "next" knappen, da den ikke er nødvendig for bugtesting lenger

--- src/main/java/mastermindgui/startController.java
+++ src/main/java/mastermindgui/startController.java
@@ -52,14 +52,7 @@ public class startController {
 
     }
     
-    @FXML
-    public void next(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("codebreaker.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    
 
 
 
