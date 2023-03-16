a5ef3de7e9dda4acfdb178fda99ab2f4920723e8
Sten Kristian Høitomt <97731056+hoitomt1@users.noreply.github.com>
2023-03-16 10:32:30 +0100

    Satt opp testlibrariet slik at man kan skrive tester

---
+++ src/test/java/mastermindgui/GuessgetTest.java
@@ -0,0 +1,35 @@
+package mastermindgui;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessgetTest {
+
+    Guessget guessget;
+
+    @BeforeEach
+    void setUp() {
+        guessget = new Guessget();
+    }
+
+    @Test
+    void testLengthCheck() {
+        assertTrue(guessget.lengthcheck("ABC"));
+        assertFalse(guessget.lengthcheck("ABCD"));
+    }
+
+    @Test
+    void testCharCheck() {
+        assertTrue(guessget.charcheck("ABCD"));
+        assertFalse(guessget.charcheck("ABZDS"));
+    }
+
+    @Test
+    void testValidityCheck() {
+        assertEquals(1, guessget.Validitycheck("ABCEG")); // contains invalid characters and has incorrect length
+        assertEquals(2, guessget.Validitycheck("ABC")); // has incorrect length
+        assertEquals(3, guessget.Validitycheck("ABCG")); // contains invalid characters
+        assertEquals(4, guessget.Validitycheck("ABCD")); // valid guess
+    }
+}
